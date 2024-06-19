bb958234017456e16ac5e92e77eae1e8068d26db
newfaji <158270331+newfaji@users.noreply.github.com>
2024-06-19 18:49:23 +0900

    .

---
+++ test_0619/js/main.js
@@ -0,0 +1,86 @@
+$(document).ready(function(){
+    const swiper = new Swiper('.popup .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        autoplay: {  /* 팝업 자동 실행 */
+            delay: 5000,
+            disableOnInteraction: true,
+        },        
+        effect: "fade", /* fade 효과 */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.popup .swiper-pagination', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+        },
+        navigation: {  /* 이전, 다음 버튼 */
+            nextEl: '.popup .swiper-button-next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.popup .swiper-button-prev',  
+        },
+    });//swiper
+
+    const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+        autoplay: {  /* 팝업 자동 실행 */
+            delay: 1000,
+            disableOnInteraction: true,
+        },
+
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.visual .paging', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+        },
+        navigation: {  /* 이전, 다음 버튼 */
+            nextEl: '.visual .btn_next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.visual .btn_prev',  
+        },
+
+    });//swiper
+
+    const book_swiper = new Swiper('.book .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: "auto", /* li의 넓이 비율로 안함 - css에서 준 넓이대로 함 */
+        spaceBetween: 16, /* li와 li사이 - 제일 작은 여백 */
+        breakpoints: {
+            640: {  /* 640px 이상이 되면 적용 */
+                spaceBetween: 24, 
+            },
+            1024: {  /* 1024px 이상이 되면 적용 */
+                spaceBetween: 24,
+            },
+        },
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        
+    });//swiper
+
+    const sns_swiper = new Swiper('.sns .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            640: {    /* 640px 이상일때 적용 */
+                slidesPerView: 4,
+                spaceBetween: 24,
+            },            
+        },
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+
+    });//swiper
+
+
+    /* 
+        .visual .stop -- 정지버튼
+        .visual .play -- 재생버튼
+        정지버튼을 클릭하면 visual_swiper 가 멈춤
+        재생버튼을 클릭하면 visual_swiper 가 autoplay
+    */
+    $(".visual .play").hide()
+    $(".visual .stop").on("click", function(){
+        visual_swiper.autoplay.stop();  /* 일시정지 기능 */
+        $(".visual .stop").hide()
+        $(".visual .play").show()
+    });
+    $(".visual .play").on("click", function(){
+        visual_swiper.autoplay.start();  
+        $(".visual .stop").show()
+        $(".visual .play").hide()
+    });
+
+})//$(document).ready
