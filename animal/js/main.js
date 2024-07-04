63e0ab01f587f8633f94cfbbb47b7e5b0695a8df
newfaji <158270331+newfaji@users.noreply.github.com>
2024-07-04 14:03:10 +0900

    .

--- animal/js/main.js
+++ animal/js/main.js
@@ -186,4 +186,15 @@ $(document).ready(function(){
         },
     });//swiper
 
+    /* family_site 열고 닫기 
+        footer .family_site button.open 를 클릭하면 family_site에 open 클래스 추가
+        footer .family_site button.close 를 클릭하면 family_site에 open 클래스 삭제
+    */
+    $('footer .family_site button.open').on('click', function(){
+        $('footer .family_site').addClass('open')
+    })
+    $('footer .family_site button.close').on('click', function(){
+        $('footer .family_site').removeClass('open')
+    })
+
 })//document
