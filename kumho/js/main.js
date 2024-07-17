1fe6c24d20a567923be9611d2dfc6f6e624249ec
newfaji <158270331+newfaji@users.noreply.github.com>
2024-07-17 13:25:35 +0900

    .

--- kumho/js/main.js
+++ kumho/js/main.js
@@ -45,4 +45,23 @@ $(document).ready(function(){
         $(this).hide() //숨김
         $('.visual .btn_wrap button.btn_stop').show() //보이기
     })
-})
+
+    /*
+        --pc일때
+        .biz .list ul li 에 마우스를 올리면
+        마우스를 올린 li는 on클래스 추가
+        마우스를 올리지 않은 li는 off클래스를 추가
+    */
+    $('.biz .list ul li').on('mouseenter', function(){
+        if($(window).width() > 1024){ //브라우저 사이즈 1025부터 적용
+            $('.biz .list ul li').removeClass('on')
+            $('.biz .list ul li').addClass('off')
+            $(this).removeClass('off')
+            $(this).addClass('on')
+        }
+    })
+    $('.biz .list').on('mouseleave', function(){
+        $('.biz .list ul li').removeClass('on')
+        $('.biz .list ul li').removeClass('off')
+    })
+})//document.ready
