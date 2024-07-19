9961f2aab5779e3579847219c9f04f9c4f3b4e09
newfaji <158270331+newfaji@users.noreply.github.com>
2024-07-19 13:13:35 +0900

    .

--- kumho/js/common.js
+++ kumho/js/common.js
@@ -99,5 +99,23 @@ $(document).ready(function(){
         $('header').removeClass('menu_open')
         $("html, body").css({overflow : "visible", height : "auto"}).unbind('scroll touchmove mousewheel');
     })
+
+    /*  
+        footer .family_site .family_open 을 클릭하면
+        1. footer .family_site에 open클래스 추가
+        2. footer .family_site ul을 열어줌
+
+        footer .family_site .family_close 를 클릭하면
+        1. footer .family_site에 open클래스 삭제
+        2. footer .family_site ul을 닫아줌
+    */
+    $('footer .family_site .family_open').on('click', function(){
+        $('footer .family_site').addClass('open')
+        $('footer .family_site ul').slideDown()
+    })
+    $('footer .family_site .family_close').on('click', function(){
+        $('footer .family_site').removeClass('open')
+        $('footer .family_site ul').slideUp()
+    })
     
 })//document.ready
