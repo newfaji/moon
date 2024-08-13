d981c7c2dc28587dcb9d14e4edc779d3cdd9b9d5
newfaji <158270331+newfaji@users.noreply.github.com>
2024-08-13 13:17:24 +0900

    0813 폴바셋

--- paulbassett/js/main.js
+++ paulbassett/js/main.js
@@ -52,4 +52,46 @@ $(document).ready(function(){
         ]*/
     })
 
+    $('.md .list .popup').slick({
+      //slidesToShow: 4, //한번에 보일 팝업 수
+      dots: false, //하단 페이지 버튼 (true, false)
+	    arrows: false,  //다음, 이전팝업 (true, false)
+      swipeToSlide: true, //드래그한만큼 슬라이드 움직이기
+      variableWidth: true, //넓이를 자유롭게 설정
+      
+    })
+    $('.event .list .popup').slick({
+      slidesToShow: 5, //한번에 보일 팝업 수
+      dots: false, //하단 페이지 버튼 (true, false)
+	    arrows: false,  //다음, 이전팝업 (true, false)
+      swipeToSlide: true, //드래그한만큼 슬라이드 움직이기
+      //variableWidth: true, //넓이를 자유롭게 설정
+      responsive: [
+        {
+            breakpoint: 1300, //1300px 이하
+            settings: {
+              slidesToShow: 4,
+            }
+        },
+        {
+            breakpoint: 1024, //1300px 이하
+            settings: {
+              slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 768, //1300px 이하
+            settings: {
+              slidesToShow: 2,
+            }
+        },
+        {
+          breakpoint: 475, //1300px 이하
+          settings: {
+            slidesToShow: 1,
+          }
+        },
+      ]
+    })
+
 })//$(document).ready
